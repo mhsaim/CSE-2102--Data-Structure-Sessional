a7c3fbfe93d42ec178ed5f1e610e066c80d4ea9a
mhsaim <mhsaim024@gmail.com>
2025-01-19 00:52:11 +0600

    Update Array_Binary_Search.cpp

--- Array/Array_Binary_Search.cpp
+++ Array/Array_Binary_Search.cpp
@@ -1,59 +1,36 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-#define PI acos(-1)
-#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define endl '\n'
+int binarySearch(int arr[], int size, int target) {
+    int low = 0, high = size - 1;
 
-typedef long long int ll;
-typedef unsigned long long int llu;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
 
-typedef vector<ll> vii;
-typedef vector<int> vi;
-
-int Binary_search(int arr[],int low,int high,int sv)
-{
-
-    while(low<=high)
-    {
-        int mid= (low+high)/2;
-
-        if(arr[mid]==sv) return mid;
-
-        else if(arr[mid]<sv) low = mid+1;
-
-        else high= mid-1;
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
     }
+
     return -1;
 }
 
-int main()
-{
-    int s;
-    cout<<"Enter the size of Array: ";
-    cin>>s;
-    int arr[s];
-    cout<<"Enter the value of array: ";
-    for(int i=0;i<s;i++)
-    {
-        cin>>arr[i];
-    }
-    cout<<"Array is: ";
-    for(int i=0;i<s;i++)
-    {
-        cout<<arr[i]<<" ";
+int main() {
+    int arr[] = {1, 3, 5, 7, 9, 11, 13, 15};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 7;
+
+    int result = binarySearch(arr, size, target);
+
+    if (result != -1) {
+        cout << "Element found at index " << result << endl;
+    } else {
+        cout << "Element not found" << endl;
     }
 
-    int len = sizeof(arr)/sizeof(arr[0]);
-    int sv;
-    cout<<endl<<"Enter the Searching Value of array: ";
-    cin>>sv;
-    int position= Binary_search(arr,0,len,sv);
-
-    if(position != -1)
-    cout<<sv<<" is present at index "<<position+1<<endl;
-    else cout<<sv<<" is not present in Array"<<endl;
-
-    //cout<<endl<<PI<<endl;
-return 0;
+    return 0;
 }
