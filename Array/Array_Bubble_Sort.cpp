bbfa9b645b76cf3173fd653b4c09ca5e40798cbe
mhsaim <mhsaim024@gmail.com>
2025-01-19 01:38:19 +0600

    Update Array_Bubble_Sort.cpp

--- Array/Array_Bubble_Sort.cpp
+++ Array/Array_Bubble_Sort.cpp
@@ -3,34 +3,37 @@ using namespace std;
 
 void bubbleSort(int arr[], int size) {
 
-    for (int leap = 0; leap < size - 1; ++leap) {
-        for (int i = 0; i < size - leap - 1; ++i) {
-            if (arr[i] > arr[i + 1]) {
-                int temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
+    for (int leap=0; leap<size-1; leap++)
+    {
+        for (int i=0; i<size-leap-1; i++)
+        {
+            if (arr[i] > arr[i+1])
+            {
+                int temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
             }
         }
     }
 }
 
 void printArray(int arr[], int size) {
-    for (int i = 0; i < size; ++i) {
-        cout << arr[i] << " ";
+    for (int i=0; i<size; i++)
+    {
+        cout<<arr[i]<<" ";
     }
-    cout << endl;
+    cout<<endl;
 }
-
 int main() {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int size = sizeof(arr) / sizeof(arr[0]);
+    int arr[]={64, 34, 25, 12, 22, 11, 90};
+    int size=sizeof(arr)/sizeof(arr[0]);
 
-    cout << "Unsorted Array: ";
-    printArray(arr, size);
+    cout <<"Unsorted Array: ";
+    printArray(arr,size);
 
-    bubbleSort(arr, size);
+    bubbleSort(arr,size);
 
-    cout << "Sorted Array in Ascending Order: ";
+    cout <<"Sorted Array in Ascending Order: ";
     printArray(arr, size);
 
     return 0;
