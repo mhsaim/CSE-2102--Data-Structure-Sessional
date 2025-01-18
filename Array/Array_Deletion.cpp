8e85e68e97d81bfea05a2292d4a5477ee98b7d0f
mhsaim <mhsaim024@gmail.com>
2025-01-18 23:09:11 +0600

    Create Array_Deletion.cpp

---
+++ Array/Array_Deletion.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void removeElement(int arr[], int &size, int index) {
+    for (int i = index; i < size - 1; ++i) {
+        arr[i] = arr[i + 1];
+    }
+    size--;
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int size = 5;
+
+    int removeindex = 2;
+
+    removeElement(arr, size, removeindex);
+
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
