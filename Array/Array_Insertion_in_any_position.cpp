0e8aab992a528d4fcfe5315c2ef577787dc29c52
mhsaim <mhsaim024@gmail.com>
2025-01-19 01:38:13 +0600

    Update Array_Insertion_in_any_position.cpp

--- Array/Array_Insertion_in_any_position.cpp
+++ Array/Array_Insertion_in_any_position.cpp
@@ -1,42 +1,45 @@
 #include <iostream>
 using namespace std;
 
-void insertAtPosition(int arr[], int& n, int element, int position) {
-    if (position < 0 || position > n) {
-        cout << "Invalid position!" << endl;
+void insertAtPosition(int arr[], int &n, int element, int position) {
+    if (position<0 || position>n)
+    {
+        cout<<"Invalid position!"<<endl;
         return;
     }
 
-    for (int i = n; i > position; i--) {
-        arr[i] = arr[i - 1];
+    for (int i=n; i>position; i--)
+    {
+        arr[i]=arr[i-1];
     }
-    arr[position] = element;
+    arr[position]=element;
     n++;
 }
 
 int main() {
     int arr[100];
-    int n = 5;
-    arr[0] = 1;
-    arr[1] = 2;
-    arr[2] = 3;
-    arr[3] = 4;
-    arr[4] = 5;
+    int n=5;
+    arr[0]=1;
+    arr[1]=2;
+    arr[2]=3;
+    arr[3]=4;
+    arr[4]=5;
 
-    int element = 10, position = 2;
+    int element=10, position=2;
 
-    cout << "Original array: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+    cout<<"Original array: ";
+    for (int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
     }
-    cout << endl;
+    cout<<endl;
     insertAtPosition(arr, n, element, position);
 
-    cout << "Array after insertion: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+    cout<<"Array after insertion: ";
+    for (int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
     }
-    cout << endl;
-
+    cout<<endl;
     return 0;
 }
