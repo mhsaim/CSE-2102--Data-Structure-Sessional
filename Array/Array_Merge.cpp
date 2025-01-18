fd253de117b466a57c93d15e6ce3223737503a19
mhsaim <mhsaim024@gmail.com>
2025-01-19 01:38:05 +0600

    Update Array_Merge.cpp

--- Array/Array_Merge.cpp
+++ Array/Array_Merge.cpp
@@ -2,44 +2,50 @@
 using namespace std;
 
 void mergeArrays(int arr1[], int size1, int arr2[], int size2, int mergedArr[]) {
-    int i = 0, j = 0, k = 0;
-    while (i < size1 && j < size2) {
-        if (arr1[i] < arr2[j]) {
-            mergedArr[k++] = arr1[i++];
-        } else {
-            mergedArr[k++] = arr2[j++];
+    int i=0, j=0, k=0;
+    while (i<size1 && j<size2)
+    {
+        if (arr1[i] < arr2[j])
+        {
+            mergedArr[k++]=arr1[i++];
+        }
+    else {
+            mergedArr[k++]=arr2[j++];
         }
     }
 
-    while (i < size1) {
-        mergedArr[k++] = arr1[i++];
+    while (i<size1)
+        {
+        mergedArr[k++]=arr1[i++];
     }
 
-    while (j < size2) {
+    while (j<size2)
+        {
         mergedArr[k++] = arr2[j++];
     }
 }
 void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
+    for (int i=0; i<size; i++)
+        {
+        cout<<arr[i]<<" ";
     }
-    cout << endl;
+    cout<<endl;
 }
 
 int main() {
-    int arr1[] = {1, 3, 5, 7};
-    int arr2[] = {2, 4, 6, 8, 10};
+    int arr1[]={1, 3, 5, 7};
+    int arr2[]={2, 4, 6, 8, 10};
 
-    int size1 = sizeof(arr1) / sizeof(arr1[0]);
-    int size2 = sizeof(arr2) / sizeof(arr2[0]);
-    int mergedSize = size1 + size2;
+    int size1=sizeof(arr1)/sizeof(arr1[0]);
+    int size2=sizeof(arr2)/sizeof(arr2[0]);
+    int mergedSize=size1+size2;
 
     int mergedArr[mergedSize];
 
     mergeArrays(arr1, size1, arr2, size2, mergedArr);
 
-    cout << "Merged Array: ";
-    printArray(mergedArr, mergedSize);
+    cout<<"Merged Array: ";
+    printArray(mergedArr,mergedSize);
 
     return 0;
 }
