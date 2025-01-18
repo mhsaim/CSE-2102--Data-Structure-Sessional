d41c7396827b0245d966e0aa5f90e115bdd35fc5
mhsaim <mhsaim024@gmail.com>
2025-01-19 01:38:21 +0600

    Update Array_Binary_Search.cpp

--- Array/Array_Binary_Search.cpp
+++ Array/Array_Binary_Search.cpp
@@ -2,34 +2,33 @@
 using namespace std;
 
 int binarySearch(int arr[], int size, int target) {
-    int low = 0, high = size - 1;
+    int low=0,high= size-1;
 
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
+    while (low<=high) {
+        int mid=low+(high-low)/2;
 
-        if (arr[mid] == target) {
+        if (arr[mid]==target) {
             return mid;
-        } else if (arr[mid] < target) {
-            low = mid + 1;
+        } else if (arr[mid]<target) {
+            low=mid+1;
         } else {
-            high = mid - 1;
+            high=mid-1;
         }
     }
-
     return -1;
 }
 
 int main() {
-    int arr[] = {1, 3, 5, 7, 9, 11, 13, 15};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int target = 7;
+    int arr[]={1, 3, 5, 7, 9, 11, 13, 15};
+    int size =sizeof(arr)/sizeof(arr[0]);
+    int target=7;
 
-    int result = binarySearch(arr, size, target);
+    int result =binarySearch(arr, size, target);
 
     if (result != -1) {
-        cout << "Element found at index " << result << endl;
+        cout<<"Element found at index "<<result<<endl;
     } else {
-        cout << "Element not found" << endl;
+        cout<<"Element not found"<<endl;
     }
 
     return 0;
